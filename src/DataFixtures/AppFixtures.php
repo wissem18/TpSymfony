95f246d2ebb05cd342fa3bfc16232a4fda9960f6
wiss <yousfiwissem224@gmail.com>
2022-05-04 18:42:52 +0100

    tahdhiret

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,6 +5,7 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
+
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
